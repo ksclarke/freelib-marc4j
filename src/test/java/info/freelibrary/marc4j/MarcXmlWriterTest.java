14461b47b4ec57be7eb50d52e97b456d3041f4ab
Kevin S. Clarke <ksclarke@gmail.com>
2015-12-13 22:50:16 -0500

    Apply upstream PR from @bsandiford

--- src/test/java/info/freelibrary/marc4j/MarcXmlWriterTest.java
+++ src/test/java/info/freelibrary/marc4j/MarcXmlWriterTest.java
@@ -49,7 +49,8 @@ public class MarcXmlWriterTest extends XMLTestCase {
             writer.write(record);
         }
         writer.close();
-        assertXMLEqual(new String(out.toByteArray()), StringUtils.read(new File("src/test/resources/summerland.xml")));
+        assertXMLEqual(new String(out.toByteArray()), StringUtils.read(new File(
+                "src/test/resources/summerland.xml")));
     }
 
     /**
@@ -88,8 +89,8 @@ public class MarcXmlWriterTest extends XMLTestCase {
         input.close();
         writer.close();
 
-        final BufferedReader testoutput =
-                new BufferedReader(new InputStreamReader(new ByteArrayInputStream(out.toByteArray()), "UTF-8"));
+        final BufferedReader testoutput = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(out
+                .toByteArray()), "UTF-8"));
         String line;
 
         while ((line = testoutput.readLine()) != null) {
@@ -101,8 +102,8 @@ public class MarcXmlWriterTest extends XMLTestCase {
                     } else if (linePart.startsWith("the grave accent in ")) {
                         assertTrue("Incorrect value for grave", linePart.equals("the grave accent in tre\u0300s"));
                     } else if (linePart.startsWith("the acute accent in ")) {
-                        assertTrue("Incorrect value for acute", linePart
-                                .equals("the acute accent in de\u0301sire\u0301e"));
+                        assertTrue("Incorrect value for acute", linePart.equals(
+                                "the acute accent in de\u0301sire\u0301e"));
                     } else if (linePart.startsWith("the circumflex in ")) {
                         assertTrue("Incorrect value for macron", linePart.equals("the circumflex in co\u0302te"));
                     } else if (linePart.startsWith("the macron in ")) {
@@ -112,8 +113,8 @@ public class MarcXmlWriterTest extends XMLTestCase {
                     } else if (linePart.startsWith("the dot above in ")) {
                         assertTrue("Incorrect value for dot above", linePart.equals("the dot above in z\u0307aba"));
                     } else if (linePart.startsWith("the dieresis (umlaut) in ")) {
-                        assertTrue("Incorrect value for umlaut", linePart
-                                .equals("the dieresis (umlaut) in Lo\u0308wenbra\u0308u"));
+                        assertTrue("Incorrect value for umlaut", linePart.equals(
+                                "the dieresis (umlaut) in Lo\u0308wenbra\u0308u"));
                     }
                 }
             }
@@ -211,8 +212,8 @@ public class MarcXmlWriterTest extends XMLTestCase {
         input.close();
         writer.close();
 
-        final BufferedReader testoutput =
-                new BufferedReader(new InputStreamReader(new ByteArrayInputStream(out.toByteArray()), "UTF-8"));
+        final BufferedReader testoutput = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(out
+                .toByteArray()), "UTF-8"));
         String line;
 
         while ((line = testoutput.readLine()) != null) {
@@ -221,32 +222,67 @@ public class MarcXmlWriterTest extends XMLTestCase {
 
                 for (int i = 0; i < lineParts.length; i++) {
                     if (lineParts[i].startsWith("the tilde in ")) {
-                        assertTrue("Incorrect normalized value for tilde accent", lineParts[i]
-                                .equals("the tilde in ma\u00F1ana"));
+                        assertTrue("Incorrect normalized value for tilde accent", lineParts[i].equals(
+                                "the tilde in ma\u00F1ana"));
                     } else if (lineParts[i].startsWith("the grave accent in ")) {
-                        assertTrue("Incorrect normalized value for grave accent", lineParts[i]
-                                .equals("the grave accent in tr\u00E8s"));
+                        assertTrue("Incorrect normalized value for grave accent", lineParts[i].equals(
+                                "the grave accent in tr\u00E8s"));
                     } else if (lineParts[i].startsWith("the acute accent in ")) {
-                        assertTrue("Incorrect normalized value for acute accent", lineParts[i]
-                                .equals("the acute accent in d\u00E9sir\u00E9e"));
+                        assertTrue("Incorrect normalized value for acute accent", lineParts[i].equals(
+                                "the acute accent in d\u00E9sir\u00E9e"));
                     } else if (lineParts[i].startsWith("the circumflex in ")) {
-                        assertTrue("Incorrect normalized value for circumflex", lineParts[i]
-                                .equals("the circumflex in c\u00F4te"));
+                        assertTrue("Incorrect normalized value for circumflex", lineParts[i].equals(
+                                "the circumflex in c\u00F4te"));
                     } else if (lineParts[i].startsWith("the macron in ")) {
-                        assertTrue("Incorrect normalized value for macron", lineParts[i]
-                                .equals("the macron in T\u014Dkyo"));
+                        assertTrue("Incorrect normalized value for macron", lineParts[i].equals(
+                                "the macron in T\u014Dkyo"));
                     } else if (lineParts[i].startsWith("the breve in ")) {
-                        assertTrue("Incorrect normalized value for breve", lineParts[i]
-                                .equals("the breve in russki\u012D"));
+                        assertTrue("Incorrect normalized value for breve", lineParts[i].equals(
+                                "the breve in russki\u012D"));
                     } else if (lineParts[i].startsWith("the dot above in ")) {
-                        assertTrue("Incorrect normalized value for dot above", lineParts[i]
-                                .equals("the dot above in \u017Caba"));
+                        assertTrue("Incorrect normalized value for dot above", lineParts[i].equals(
+                                "the dot above in \u017Caba"));
                     } else if (lineParts[i].startsWith("the dieresis (umlaut) in ")) {
-                        assertTrue("Incorrect normalized value for umlaut", lineParts[i]
-                                .equals("the dieresis (umlaut) in L\u00F6wenbr\u00E4u"));
+                        assertTrue("Incorrect normalized value for umlaut", lineParts[i].equals(
+                                "the dieresis (umlaut) in L\u00F6wenbr\u00E4u"));
                     }
                 }
             }
         }
+    }
+
+    @Test
+    public void testMarcXmlWriterBadCharacters() throws Exception {
+        final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+        final InputStream input = getClass().getResourceAsStream(
+                StaticTestRecords.RESOURCES_BAD_CHARACTERS_IN_VARIOUS_FIELDS_MRC);
+        assertNotNull(input);
+        final MarcXmlWriter writer = new MarcXmlWriter(out, true);
+        writer.setConverter(new AnselToUnicode());
+        final MarcStreamReader reader = new MarcStreamReader(input);
+        while (reader.hasNext()) {
+            final Record record = reader.next();
+            writer.write(record);
+        }
+        input.close();
+        writer.close();
+        final BufferedReader testoutput = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(out
+                .toByteArray()), "UTF-8"));
+        String line;
+        while ((line = testoutput.readLine()) != null) {
+            // Invalid char in leader.
+            if (line.matches("[ ]*<leader>.*")) {
+                assertTrue(line.contains(">01899cam &lt;U+0014&gt;22004458a 4500<"));
+            } else if (line.matches("[ ]*<datafield tag=\"010\".*")) {
+                // Invalid char in indicators
+                assertTrue(line.contains("ind1=\"&lt;U+0014&gt;\""));
+                assertTrue(line.contains("ind2=\"&lt;U+0014&gt;\""));
+            } else if (line.matches("[ ]*2011035923.*")) {
+                // Invalid char in subfield name and subfield text
+                assertTrue(line.contains("<subfield code=\"&lt;U+0014&gt;\">&lt;U+0014&gt; 2011035923</subfield>"));
+            }
+        }
+        testoutput.close();
     }
 }
--- src/test/java/info/freelibrary/marc4j/utils/StaticTestRecords.java
+++ src/test/java/info/freelibrary/marc4j/utils/StaticTestRecords.java
@@ -1,33 +1,59 @@
+
 package info.freelibrary.marc4j.utils;
 
 import org.marc4j.marc.MarcFactory;
 import org.marc4j.marc.Record;
 
-
 public class StaticTestRecords {
+
     public static final String RESOURCES_ILLEGAL_MARC_IN_JSON_JSON = "/illegal-marc-in-json.json";
+
     public static final String RESOURCES_MARC_IN_JSON_JSON = "/marc-in-json.json";
+
     public static final String RESOURCES_MARC_JSON_JSON = "/marc-json.json";
+
     public static final String RESOURCES_LEGAL_JSON_MARC_IN_JSON_JSON = "/legal-json-marc-in-json.json";
+
     public static final String RESOURCES_BAD_LEADERS_10_11_MRC = "/bad_leaders_10_11.mrc";
+
     public static final String RESOURCES_BAD_TOO_LONG_PLUS_2_MRC = "/bad_too_long_plus_2.mrc";
+
     public static final String RESOURCES_SUMMERLAND_MRC = "/summerland.mrc";
+
     public static final String RESOURCES_SUMMERLAND_XML = "/summerland.xml";
+
     public static final String RESOURCES_SUMMERLAND_MARC_IN_JSON_JSON = "/summerland-marc-in-json.json";
-    public static final String RESOURCES_SUMMERLAND_MARC_IN_JSON_INDENTED_JSON = "/summerland-marc-in-json-indented.json";
+
+    public static final String RESOURCES_SUMMERLAND_MARC_IN_JSON_INDENTED_JSON =
+            "/summerland-marc-in-json-indented.json";
+
     public static final String RESOURCES_SUMMERLAND_MARC_JSON_JSON = "/summerland-marc-json.json";
+
     public static final String RESOURCES_SUMMERLAND_INDENTED_MARC_JSON_JSON = "/summerland-indented-marc-json.json";
+
     public static final String RESOURCES_BRKRTEST_MRC = "/brkrtest.mrc";
+
     public static final String RESOURCES_CHABON_MRC = "/chabon.mrc";
+
     public static final String RESOURCES_CHABON_XML = "/chabon.xml";
-    public static final String RESOURCES_OCLC814388508_XML = "/OCLC_814388508.xml";    
+
+    public static final String RESOURCES_OCLC814388508_XML = "/OCLC_814388508.xml";
+
     public static final String RESOURCES_CYRILLIC_CAPITAL_E_MRC = "/cyrillic_capital_e.mrc";
+
     public static final String RESOURCES_GREEK_MISSING_CHARSET_MRC = "/greekmissingcharsetchange.mrc";
+
     public static final String RESOURCES_CHINESE_MANGLED_MULTIBYTE_MRC = "/chinese_mangled_multibyte.mrc";
+
     public static final String RESOURCES_BAD_NUMERIC_CHARACTER_REFERENCE_MRC = "/loongboonmee.mrc";
 
+    public static final String RESOURCES_BAD_CHARACTERS_IN_VARIOUS_FIELDS_MRC =
+            "/bad-characters-in-various-fields.mrc";
+
     public static Record chabon[] = new Record[2];
+
     public static Record summerland[] = new Record[1];
+
     private static MarcFactory factory;
 
     static {
@@ -39,11 +65,15 @@ public class StaticTestRecords {
         chabon[0].addVariableField(factory.newDataField("020", ' ', ' ', "a", "0679450041 (acid-free paper)"));
         chabon[0].addVariableField(factory.newDataField("040", ' ', ' ', "a", "DLC", "c", "DLC", "d", "DLC"));
         chabon[0].addVariableField(factory.newDataField("100", '1', ' ', "a", "Chabon, Michael."));
-        chabon[0].addVariableField(factory.newDataField("245", '1', '4', "a", "The amazing adventures of Kavalier and Clay :", "b", "a novel /", "c", "Michael Chabon."));
-        chabon[0].addVariableField(factory.newDataField("260", ' ', ' ', "a", "New York :", "b", "Random House,", "c", "c2000."));
+        chabon[0].addVariableField(factory.newDataField("245", '1', '4', "a",
+                "The amazing adventures of Kavalier and Clay :", "b", "a novel /", "c", "Michael Chabon."));
+        chabon[0].addVariableField(factory.newDataField("260", ' ', ' ', "a", "New York :", "b", "Random House,", "c",
+                "c2000."));
         chabon[0].addVariableField(factory.newDataField("300", ' ', ' ', "a", "639 p. ;", "c", "25 cm."));
-        chabon[0].addVariableField(factory.newDataField("650", ' ', '0', "a", "Comic books, strips, etc.", "x", "Authorship", "v", "Fiction."));
-        chabon[0].addVariableField(factory.newDataField("650", ' ', '0', "a", "Heroes in mass media", "v", "Fiction."));
+        chabon[0].addVariableField(factory.newDataField("650", ' ', '0', "a", "Comic books, strips, etc.", "x",
+                "Authorship", "v", "Fiction."));
+        chabon[0].addVariableField(factory.newDataField("650", ' ', '0', "a", "Heroes in mass media", "v",
+                "Fiction."));
         chabon[0].addVariableField(factory.newDataField("650", ' ', '0', "a", "Czech Americans", "v", "Fiction."));
         chabon[0].addVariableField(factory.newDataField("651", ' ', '0', "a", "New York (N.Y.)", "v", "Fiction."));
         chabon[0].addVariableField(factory.newDataField("650", ' ', '0', "a", "Young men", "v", "Fiction."));
@@ -59,11 +89,14 @@ public class StaticTestRecords {
         chabon[1].addVariableField(factory.newDataField("020", ' ', ' ', "a", "0786816155 (pbk.)"));
         chabon[1].addVariableField(factory.newDataField("040", ' ', ' ', "a", "DLC", "c", "DLC", "d", "DLC"));
         chabon[1].addVariableField(factory.newDataField("100", '1', ' ', "a", "Chabon, Michael."));
-        chabon[1].addVariableField(factory.newDataField("245", '1', '0', "a", "Summerland /", "c", "Michael Chabon."));
+        chabon[1].addVariableField(factory.newDataField("245", '1', '0', "a", "Summerland /", "c",
+                "Michael Chabon."));
         chabon[1].addVariableField(factory.newDataField("250", ' ', ' ', "a", "1st ed."));
-        chabon[1].addVariableField(factory.newDataField("260", ' ', ' ', "a", "New York :", "b", "Miramax Books/Hyperion Books for Children,", "c", "c2002."));
+        chabon[1].addVariableField(factory.newDataField("260", ' ', ' ', "a", "New York :", "b",
+                "Miramax Books/Hyperion Books for Children,", "c", "c2002."));
         chabon[1].addVariableField(factory.newDataField("300", ' ', ' ', "a", "500 p. ;", "c", "22 cm."));
-        chabon[1].addVariableField(factory.newDataField("520", ' ', ' ', "a", "Ethan Feld, the worst baseball player in the history of the game, finds himself recruited by a 100-year-old scout to help a band of fairies triumph over an ancient enemy."));
+        chabon[1].addVariableField(factory.newDataField("520", ' ', ' ', "a",
+                "Ethan Feld, the worst baseball player in the history of the game, finds himself recruited by a 100-year-old scout to help a band of fairies triumph over an ancient enemy."));
         chabon[1].addVariableField(factory.newDataField("650", ' ', '1', "a", "Fantasy."));
         chabon[1].addVariableField(factory.newDataField("650", ' ', '1', "a", "Baseball", "v", "Fiction."));
         chabon[1].addVariableField(factory.newDataField("650", ' ', '1', "a", "Magic", "v", "Fiction."));
@@ -72,7 +105,7 @@ public class StaticTestRecords {
     }
 
     private static Record makeSummerlandRecord() {
-        Record sumland = factory.newRecord("00714cam a2200205 a 4500");
+        final Record sumland = factory.newRecord("00714cam a2200205 a 4500");
         sumland.addVariableField(factory.newControlField("001", "12883376"));
         sumland.addVariableField(factory.newControlField("005", "20030616111422.0"));
         sumland.addVariableField(factory.newControlField("008", "020805s2002    nyu    j      000 1 eng  "));
@@ -82,15 +115,16 @@ public class StaticTestRecords {
         sumland.addVariableField(factory.newDataField("100", '1', ' ', "a", "Chabon, Michael."));
         sumland.addVariableField(factory.newDataField("245", '1', '0', "a", "Summerland /", "c", "Michael Chabon."));
         sumland.addVariableField(factory.newDataField("250", ' ', ' ', "a", "1st ed."));
-        sumland.addVariableField(factory.newDataField("260", ' ', ' ', "a", "New York :", "b", "Miramax Books/Hyperion Books for Children,", "c", "c2002."));
+        sumland.addVariableField(factory.newDataField("260", ' ', ' ', "a", "New York :", "b",
+                "Miramax Books/Hyperion Books for Children,", "c", "c2002."));
         sumland.addVariableField(factory.newDataField("300", ' ', ' ', "a", "500 p. ;", "c", "22 cm."));
-        sumland.addVariableField(factory.newDataField("520", ' ', ' ', "a", "Ethan Feld, the worst baseball player in the history of the game, finds himself recruited by a 100-year-old scout to help a band of fairies triumph over an ancient enemy."));
+        sumland.addVariableField(factory.newDataField("520", ' ', ' ', "a",
+                "Ethan Feld, the worst baseball player in the history of the game, finds himself recruited by a 100-year-old scout to help a band of fairies triumph over an ancient enemy."));
         sumland.addVariableField(factory.newDataField("650", ' ', '1', "a", "Fantasy."));
         sumland.addVariableField(factory.newDataField("650", ' ', '1', "a", "Baseball", "v", "Fiction."));
         sumland.addVariableField(factory.newDataField("650", ' ', '1', "a", "Magic", "v", "Fiction."));
         return sumland;
     }
-
 
     public static Record getSummerlandRecord() {
         return makeSummerlandRecord();
